aed02ec3dc020053c11cf064e359af12c71624d3
Rafayet Hossen <hossenmdrafayet@gmail.com>
2025-08-19 19:09:02 +0600

    scene change functionality between loing and create account page

--- src/main/java/com/example/zcare/Controller/CreateAccountController.java
+++ src/main/java/com/example/zcare/Controller/CreateAccountController.java
@@ -1,4 +1,27 @@
 package com.example.zcare.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class CreateAccountController {
+    @FXML
+    private Button backBtn;
+    @FXML
+    private Button createAccountBtn;
+
+    public void switchToLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/zcare/FXML/login.fxml")));
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- src/main/java/com/example/zcare/Controller/LoginController.java
+++ src/main/java/com/example/zcare/Controller/LoginController.java
@@ -1,14 +1,27 @@
 package com.example.zcare.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
-
+    private Button signupBtn;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button signInBtn;
+
+    public void switchToCreateAccount(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/zcare/FXML/createAccount.fxml")));
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
